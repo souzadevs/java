88771b0b6d6c8c1d6ff6fbf2221c5099e7adcf13
Bruno Araujo <eletronica.bruno@hotmail.com>
2020-08-23 19:40:07 -0300

    Iniciando terceiro exercício

--- Java_S13e2/src/app/Java_S13e2.java
+++ Java_S13e2/src/app/Java_S13e2.java
@@ -5,6 +5,9 @@
  */
 package app;
 
+import java.util.Scanner;
+import model.entities.PostModel;
+
 /**
  *
  * @author Bruno Araujo
@@ -17,6 +20,31 @@ public class Java_S13e2
      */
     public static void main(String[] args)
     {
+        PostModel postModel = new PostModel();
+        
+        System.out.println("Título do post: ");
+        String title = new Scanner(System.in).nextLine();
+        
+        System.out.println("Cinteúdo do post: ");
+        String content = new Scanner(System.in).nextLine();
+        
+        System.out.println("Quantidade de likes: ");
+        long likes = Long.parseLong(new Scanner(System.in).nextLine());
+        
+        boolean isCommenting = true;
+        System.out.println("Deseja fazer algum comentário no post? S/N");
+        String answer = new Scanner(System.in).nextLine();
+        if(answer.equals("S")){
+            isCommenting = true;
+        } else if(answer.equals("N")){
+            isCommenting = false;
+        } else {
+            System.out.println("Digite apenas S ou N - Sim ou Não: ");
+        }
+        
+        System.out.println("Comentário: ");
+        String comment = new Scanner(System.in).nextLine();
+        
         
     }
     
